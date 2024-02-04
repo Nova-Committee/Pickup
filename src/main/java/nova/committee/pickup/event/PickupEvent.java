76877a542b487ed1a7af68442e93daffca835b44
chemiofitor <98449295+chemiofitor@users.noreply.github.com>
2024-02-04 19:01:50 +0800

    update to 1.18.2

--- src/main/java/nova/committee/pickup/event/PickupEvent.java
+++ src/main/java/nova/committee/pickup/event/PickupEvent.java
@@ -1,12 +1,10 @@
 package nova.committee.pickup.event;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.item.ItemFrameEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResultType;
-import net.minecraftforge.event.entity.item.ItemEvent;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,25 +13,23 @@ import net.minecraftforge.fml.common.Mod;
 public class PickupEvent {
     @SubscribeEvent
     public static void entityInteract(PlayerInteractEvent rawEvent) {
-        if (rawEvent instanceof PlayerInteractEvent.EntityInteract) {
-            PlayerInteractEvent.EntityInteract event = (PlayerInteractEvent.EntityInteract) rawEvent;
-            PlayerEntity player = event.getPlayer();
+        if (rawEvent instanceof PlayerInteractEvent.EntityInteract event) {
+            Player player = event.getPlayer();
             Entity target = event.getTarget();
             ItemStack hand = event.getItemStack();
-            if (target instanceof ItemFrameEntity) {
-                ItemFrameEntity frameEntity = (ItemFrameEntity) target;
+            if (target instanceof ItemFrame frameEntity) {
                 ItemStack inside = frameEntity.getItem().copy();
                 if (!inside.isEmpty()) {
                     if (hand.isEmpty()) {
                         player.setItemInHand(event.getHand(), inside);
                         frameEntity.setItem(ItemStack.EMPTY);
-                        event.setCancellationResult(ActionResultType.SUCCESS);
+                        event.setCancellationResult(InteractionResult.SUCCESS);
                         event.setCanceled(true);
                     } else if (!hand.isEmpty() && player.isShiftKeyDown()) {
                         if (!inside.equals(hand, true)) {
                             frameEntity.setItem(hand.split(1));
                             player.addItem(inside);
-                            event.setCancellationResult(ActionResultType.SUCCESS);
+                            event.setCancellationResult(InteractionResult.SUCCESS);
                             event.setCanceled(true);
                         }
                     }
